79e94f1510931d0b4068a0a18caa721dc1dfd823
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-03 19:05:29 -0600

    V2

--- java/Ejer3.java
+++ java/Ejer3.java
@@ -1,6 +1,17 @@
-package java;
+import java.util.Scanner;
+
 public class Ejer3 {
     public static void main(String[] args) {
-        
+        double pi = 3.1416;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el radio del circulo");
+        double radio = scanner.nextDouble();
+
+        double operacion = pi * (radio * radio);
+
+        System.out.println("El área del círculo es: " + operacion);
+
+        scanner.close();
     }
 }
