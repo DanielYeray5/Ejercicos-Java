3d5c65100b6433a08d4e54613f2efd6e3a3c4c67
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-04 16:11:13 -0600

    Dia 3

--- Ejer5.java
+++ Ejer5.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Ejer5 {
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un numero a dividir");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0){
+            System.out.println("El numero " + numero + " es divisible entre 2");
+        }else{
+            System.out.println("El numero " + numero + " no es divisible entre 2");
+        }
+
+        scanner.close();
     }
 }
