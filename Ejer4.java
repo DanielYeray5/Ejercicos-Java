ff4ea0a4a4b976aadf901f22a38a7efafc3cdb5a
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-03 19:14:23 -0600

    V3

--- Ejer4.java
+++ Ejer4.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Ejer4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el precio del producto");
+        double precio = scanner.nextDouble();
+        double iva = 0.21;
+        double resultado = precio + (precio * iva);
+        System.out.println("El resultado con IVA es: " + resultado);
         
+        scanner.close();
     }
 }
