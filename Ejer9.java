803eea5928553fa085666a1d46d52b6f51b9c5e3
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-05 17:40:25 -0600

    Dia 4

--- Ejer9.java
+++ Ejer9.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class Ejer9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa el número de ventas");
+        System.out.println("Ingresa el número de ventas ");
         int numVentas = scanner.nextInt();
 
         int sumVentas = 0;
         for (int i = 0; i < numVentas; i++){
-            System.out.println("Ingresa el precio de la venta" + (i + 1));
+            System.out.println("Ingresa el precio de la venta " + (i + 1) + ":");
             int venta = scanner.nextInt();
 
             sumVentas += venta;
         }
-        System.out.println(sumVentas);
+        System.out.println("El total de las ventas es de: " + sumVentas);
         scanner.close();
     }
 }
