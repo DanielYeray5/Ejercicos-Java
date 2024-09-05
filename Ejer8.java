270cee26cba96fb2cee8951418ad4de7dde83cab
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-05 07:13:24 -0600

    Dia 4

--- Ejer8.java
+++ Ejer8.java
@@ -1,5 +1,11 @@
 public class Ejer8 {
     public static void main(String[] args) {
-        
+        double x = 1;
+        for(int i = 0; x <= 100; i++){
+            if(x % 2 == 0 || x % 3 == 0){
+                System.out.println(x);
+            }
+            x++;
+        }
     }
 }
