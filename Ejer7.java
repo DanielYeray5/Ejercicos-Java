2e0ee2c5ad5cc8874f3d57921bf167d6ae93edae
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-05 07:01:36 -0600

    Dia 4

---
+++ Ejer7.java
@@ -0,0 +1,11 @@
+public class Ejer7 {
+    public static void main(String[] args) {
+        int numero = 0;
+
+        for(int i = 0; numero < 101; i++) {
+            System.out.println(numero);
+            numero++;
+        }
+
+    }
+}
