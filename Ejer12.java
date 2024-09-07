6bd4e5ee4dafb5f72a4b820ed843adaf03779647
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-06 19:12:48 -0600

    Dia 5

--- Ejer12.java
+++ Ejer12.java
@@ -1,5 +1,19 @@
+
+import javax.swing.JOptionPane;
+
 public class Ejer12 {
+
     public static void main(String[] args) {
-        
+        String texto1 = JOptionPane.showInputDialog("Introduce el primero numero");
+        int num1 = Integer.parseInt(texto1);
+
+        String texto2 = JOptionPane.showInputDialog("Introduce el segundo numero");
+        int num2 = Integer.parseInt(texto2);
+
+        for (int i = 0; i < 10; i++) {
+            //Generamos un numero aleatorio
+            int numAleatorio = (int) Math.floor(Math.random() * (num1 - num2) + num2);
+            System.out.println(numAleatorio);
+        }
     }
 }
