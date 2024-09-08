82431cca0db93b63be9a07a643c3923ccd888a1a
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-07 19:10:54 -0600

    Dia 6

---
+++ Ejer14.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Ejer14 {
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Inserta un numero entero mayor que 1");
+        int num;
+
+        do {
+            num = sn.nextInt();
+            if (num <= 1) {
+                System.out.println("Error. No has introducido un valor correcto."
+                        + "Vuelve a intentarlo");
+            }
+        } while (!(num >= 1)); 
+        int suma = 0;
+        for (int contador = 1; contador <= num; contador++) {
+            suma += contador;
+        }
+        System.out.println("La suma es: " + suma);
+
+    }
+}
