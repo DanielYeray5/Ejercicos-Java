8ce0e5b20066b739e1681db0b3a0f904835bf362
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-07 19:08:46 -0600

    Dia 6

---
+++ Ejer13.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Ejer13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int numero = sc.nextInt();
+        if (numero <= 1) {
+            System.out.println("El numero " + numero + " no es primo");
+        } else {
+            int raiz = (int) Math.sqrt(numero);
+            int contador = 0;
+            for (int i = raiz; i > 1; i--) {
+                if (numero % i == 0) {
+                    contador++;
+                }
+            }
+            if (contador < 1) {
+                System.out.println("El numero " + numero + " es primo");
+            } else {
+                System.out.println("El numero " + numero + " no es primo");
+            }
+        }
+        sc.close();
+    }
+}
