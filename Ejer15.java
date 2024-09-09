c75bb8e4b1af00a294a9c819e136d4f925fbac2b
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-08 18:48:05 -0600

    Dia 7

--- Ejer15.java
+++ Ejer15.java
@@ -1,5 +1,30 @@
+
+import javax.swing.JOptionPane;
+
 public class Ejer15 {
+
     public static void main(String[] args) {
-        
+        String texto = JOptionPane.showInputDialog(null,
+                "Introduce un texto, cadena vacia para terminar",
+                "Introducir texto",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        String cadenaResultante = "";
+
+        while (!texto.isEmpty()) {
+
+            cadenaResultante += texto;
+
+            texto = JOptionPane.showInputDialog(null,
+                    "Introduce un texto, cadena vacia para terminar",
+                    "Introducir texto",
+                    JOptionPane.INFORMATION_MESSAGE);
+
+        }
+
+        JOptionPane.showMessageDialog(null,
+                cadenaResultante,
+                "Resultado",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
